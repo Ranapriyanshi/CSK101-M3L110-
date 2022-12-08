68eee75b02deb06288e3c001c7190d236e89ae06
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2022-12-08 15:27:20 +0530

    2 files modified

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // Progression 1: create a function and fetch the api using axios
+fetch('https://gnews.io/api/v4/top-headlines?token=b592c9edc1f63e05142fd3d34e1235f4&lang=en').then((data)=>{
+  return data.json();
+}).then((priyanshi)=>{
+  let data="";
+  for(i=0;i<10;i++){
+    data+=` <div class="card">
+    <h1 class="title">${priyanshi.articles[i].title}</h1>
+    <img src="${priyanshi.articles[i].image}" alt="img">
+    <p>${priyanshi.articles[i].content}</p>
+  </div>`
+  }
+  document.getElementById("cards").innerHTML = data
+}).catch((error)=>{
+  console.log("Error")
+})
